202f591b1d1e663db02f798e86d14967f1a6dc0a
Vlad00k <veselov.vladiken@mail.ru>
2023-07-07 00:00:59 +0700

    add req

--- common_scripts/config_db.py
+++ common_scripts/config_db.py
@@ -1,5 +1,5 @@
 DB_POLYANALYST = 'postgresql://polyanalyst:EY5gKg7EBma6jrQY@172.33.2.100:5432/polyanalyst'   #LOCAL  
 DB_POLYANALYST = 'postgresql://polyanalyst:EY5gKg7EBma6jrQY@185.127.150.35:6333/polyanalyst' #REMOTE
 
-DB_COURSES = 'postgresql://courses:4Ao40sWSwY4wbFcQ@172.33.2.151:6601/courses'     #LOCAL
+# DB_COURSES = 'postgresql://courses:4Ao40sWSwY4wbFcQ@172.33.2.151:6601/courses'     #LOCAL
 DB_COURSES = 'postgresql://courses:4Ao40sWSwY4wbFcQ@185.127.150.35:6601/courses'   #REMOTE
